b176b6edb483025ed5b1af5e08ef70b1b6213cef
zhaokai <506683059@qq.com>
2019-05-18 08:26:01 +0800

    增加获取服务提供者实例demo

--- spring-cloud-ribbon/src/main/java/com/github/zk/springcloudribbon/controller/RibbonController.java
+++ spring-cloud-ribbon/src/main/java/com/github/zk/springcloudribbon/controller/RibbonController.java
@@ -2,6 +2,8 @@ package com.github.zk.springcloudribbon.controller;
 
 import com.github.zk.springcloudribbon.service.IRibbon;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class RibbonController {
     @Autowired
     private IRibbon iRibbon;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
     @RequestMapping("/hi")
     public String hiController(@RequestParam String name) {
         return iRibbon.hiService(name);
+    }
+    @RequestMapping("/testRibbon")
+    public String testController() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        return serviceInstance.getHost() + ":" + serviceInstance.getPort();
     }
 }
